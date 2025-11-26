c98b886fe2b0f9f0465fd902f3ace349d77e4fc7
ch06hro <chrischi27.1@gmail.com>
2025-11-26 13:15:14 +0000

    aufg 1,2,3

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,19 @@
+fourEqual:: Int->Int->Int->Int->Bool
+fourEqual a b c d= if a==b && b==c &&c==d then True else False
+
+orderTriple:: (Int, Int, Int)-> (Int, Int, Int)
+orderTriple (a,b,c)=
+    let x= min a (min b c) 
+        z= max a (max b c)
+        y= a+b+c-x-z
+    in (x,y,z)
+
+fib:: Int-> Int
+fib n
+    |n==0 =0
+    |n==1 =1
+    |otherwise =fib (n-1)+ fib(n-2)
+
 isElem :: Int -> [Int] -> Bool
 isElem _ [] = False
 isElem e (x:xs)
